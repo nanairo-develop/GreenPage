aeaf1ef288974c09d6f0c335f635b3c3194d0d98
nanairo-develop <nanairo.develop@gmail.com>
2023-03-09 12:09:13 +0900

    綴り間違い

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var express = require('express'),
   app = express();
 
-app.use(express.static(__dirname + 'docs/'));
+app.use(express.static(__dirname + '/docs'));
 
 var port = process.env.PORT || 8080;
 app.listen(port);
