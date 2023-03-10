6490a6450ca659baee023895725a5c053bf54a23
nanairo-develop <nanairo.develop@gmail.com>
2023-03-10 12:48:08 +0900

    アイコンのディレクトリ変更、その他微調整

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-//. app.js
-var express = require('express'),
-  app = express();
+const express = require('express');
+const app = express();
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, access_token');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' === req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
 
 app.use(express.static(__dirname + '/docs'));
 
 var port = process.env.PORT || 8080;
-app.listen(port);
-console.log('server starting on ' + port + ' ...');
+app.set('port', port);
 
-app.get('/', (req, res, next) => {
-  res.set({ 'Access-Control-Allow-Origin': '*' });
-});
+app.listen(port, () => console.log(`App started on port ${port}.`));
